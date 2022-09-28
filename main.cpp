717720a5e25f5fe2aa17cbd3077724d10aa98922
Диков Александр <112902848+DikovAlexandr@users.noreply.github.com>
2022-09-28 22:37:46 +0300

    solver added

--- main.cpp
+++ main.cpp
@@ -7,6 +7,49 @@
 
 using namespace std;
 
+void massCase(vector<double> massesOfSubstance, vector<string> intoSubstance, int num) {
+    vector<double> moleFractions(num);
+    vector<double> massElements(num);
+    vector<double> moleElements(num);
+    vector<double> massFractions(num);
+    vector<double> n(num);
+    double massOfAll = 0;
+    cout << "Enter the mole fractions of substances in the same order" << endl;
+    for (int i = 0; i < num; i++) cin >> moleFractions[i];
+    n[0] = 1; // We consider the amount of one of the substances to be equal to one
+    n[1] = 1 / moleFractions[1] - n[0];
+    for (int i = 0; i < num; i++) {
+        massElements[i] = n[i] * massesOfSubstance[i];
+        massOfAll += massElements[i];
+    }
+    for (int i = 0; i < num; i++) {
+        massFractions[i] = massElements[i] / massOfAll;
+        cout << intoSubstance[i] << " has mass fraction " << massFractions[i] << endl;
+    }
+}
+
+void moleCase(vector<double> massesOfSubstance, vector<string> intoSubstance, int num) {
+    vector<double> moleFractions(num);
+    vector<double> massElements(num);
+    vector<double> moleElements(num);
+    vector<double> massFractions(num);
+    vector<double> n(num);
+    double moleOfAll = 0;
+    cout << "Enter the mass fractions of substances in the same order" << endl;
+    for (int i = 0; i < num; i++) cin >> massFractions[i];
+    massElements[0] = 100 * massFractions[0];
+    massElements[1] = 100 - massFractions[1];
+    for (int i = 0; i < num; i++) {
+        moleElements[i] = massElements[i] / massesOfSubstance[i];
+        moleOfAll += moleElements[i];
+    }
+    for (int i = 0; i < num; i++) {
+        massFractions[i] = massElements[i] / moleOfAll;
+        cout << intoSubstance[i] << " has mole fraction " << massFractions[i] << endl;
+    }
+}
+
+
 // Разбиение формулы поэлементно с учетом коэффициентов
 vector<pair<string, int>> splitter(string s) {
     string formula = s;
@@ -57,140 +100,141 @@ double getMass(const vector<pair<string, int>> &elements, const map<string, doub
 }
 
 int main() {
-    vector<string> substance;
-    vector<double> masses;
-    int n;
-    map<string, double> element_m;
+    vector<string> intoSubstance;
+    vector<double> massesOfSubstance;
+    string plug;
+    int num;
+    map<string, double> elementAtomicMass;
     {
-        element_m["Ac"] = 227.028;
-        element_m["Ag"] = 107.868;
-        element_m["Al"] = 26.982;
-        element_m["Am"] = 243.061;
-        element_m["Ar"] = 39.948;
-        element_m["As"] = 74.922;
-        element_m["At"] = 209.987;
-        element_m["Au"] = 196.967;
-        element_m["B"] = 10.811;
-        element_m["Ba"] = 137.33;
-        element_m["Be"] = 9.012;
-        element_m["Bi"] = 208.98;
-        element_m["Bk"] = 247.07;
-        element_m["Br"] = 79.904;
-        element_m["C"] = 12.011;
-        element_m["Ca"] = 40.078;
-        element_m["Cd"] = 112.41;
-        element_m["Ce"] = 140.12;
-        element_m["Cf"] = 251.08;
-        element_m["Cl"] = 35.453;
-        element_m["Cm"] = 247.07;
-        element_m["Co"] = 58.933;
-        element_m["Cr"] = 51.996;
-        element_m["Cs"] = 132.905;
-        element_m["Cu"] = 63.546;
-        element_m["Dy"] = 162.5;
-        element_m["Er"] = 167.26;
-        element_m["Es"] = 252.083;
-        element_m["Eu"] = 151.96;
-        element_m["F"] = 18.998;
-        element_m["Fe"] = 55.847;
-        element_m["Fm"] = 257.095;
-        element_m["Fr"] = 223.02;
-        element_m["Ga"] = 69.723;
-        element_m["Gd"] = 157.25;
-        element_m["Ge"] = 72.59;
-        element_m["H"] = 1.008;
-        element_m["He"] = 4.003;
-        element_m["Hf"] = 178.49;
-        element_m["Hg"] = 200.59;
-        element_m["Ho"] = 164.93;
-        element_m["I"] = 126.905;
-        element_m["In"] = 114.82;
-        element_m["Ir"] = 192.22;
-        element_m["K"] = 39.098;
-        element_m["Kr"] = 83.8;
-        element_m["La"] = 138.906;
-        element_m["Li"] = 6.941;
-        element_m["Lr"] = 260.105;
-        element_m["Lu"] = 174.967;
-        element_m["Md"] = 258.099;
-        element_m["Mg"] = 24.305;
-        element_m["Mn"] = 54.938;
-        element_m["Mo"] = 95.94;
-        element_m["N"] = 14.007;
-        element_m["Na"] = 22.99;
-        element_m["Nb"] = 92.906;
-        element_m["Nd"] = 144.24;
-        element_m["Ne"] = 20.179;
-        element_m["Ni"] = 58.69;
-        element_m["No"] = 259.101;
-        element_m["Np"] = 237.048;
-        element_m["O"] = 15.999;
-        element_m["Os"] = 190.2;
-        element_m["P"] = 30.974;
-        element_m["Pa"] = 231.036;
-        element_m["Pb"] = 207.2;
-        element_m["Pd"] = 106.42;
-        element_m["Pm"] = 144.913;
-        element_m["Po"] = 208.982;
-        element_m["Pr"] = 140.908;
-        element_m["Pt"] = 195.08;
-        element_m["Pu"] = 244.064;
-        element_m["Ra"] = 226.025;
-        element_m["Rb"] = 85.468;
-        element_m["Re"] = 186.207;
-        element_m["Rh"] = 102.906;
-        element_m["Rn"] = 222.018;
-        element_m["Ru"] = 101.07;
-        element_m["S"] = 32.066;
-        element_m["Sb"] = 121.75;
-        element_m["Sc"] = 44.956;
-        element_m["Se"] = 78.96;
-        element_m["Si"] = 28.086;
-        element_m["Sm"] = 150.36;
-        element_m["Sn"] = 118.71;
-        element_m["Sr"] = 87.62;
-        element_m["Ta"] = 180.948;
-        element_m["Tb"] = 158.925;
-        element_m["Tc"] = 97.907;
-        element_m["Te"] = 127.6;
-        element_m["Th"] = 232.038;
-        element_m["Ti"] = 47.88;
-        element_m["Tl"] = 204.383;
-        element_m["Tm"] = 168.934;
-        element_m["U"] = 238.029;
-        element_m["V"] = 50.942;
-        element_m["W"] = 183.85;
-        element_m["Xe"] = 131.29;
-        element_m["Y"] = 88.906;
-        element_m["Yb"] = 173.04;
-        element_m["Zn"] = 65.39;
-        element_m["Zr"] = 91.224;
+        elementAtomicMass["Ac"] = 227.028;
+        elementAtomicMass["Ag"] = 107.868;
+        elementAtomicMass["Al"] = 26.982;
+        elementAtomicMass["Am"] = 243.061;
+        elementAtomicMass["Ar"] = 39.948;
+        elementAtomicMass["As"] = 74.922;
+        elementAtomicMass["At"] = 209.987;
+        elementAtomicMass["Au"] = 196.967;
+        elementAtomicMass["B"] = 10.811;
+        elementAtomicMass["Ba"] = 137.33;
+        elementAtomicMass["Be"] = 9.012;
+        elementAtomicMass["Bi"] = 208.98;
+        elementAtomicMass["Bk"] = 247.07;
+        elementAtomicMass["Br"] = 79.904;
+        elementAtomicMass["C"] = 12.011;
+        elementAtomicMass["Ca"] = 40.078;
+        elementAtomicMass["Cd"] = 112.41;
+        elementAtomicMass["Ce"] = 140.12;
+        elementAtomicMass["Cf"] = 251.08;
+        elementAtomicMass["Cl"] = 35.453;
+        elementAtomicMass["Cm"] = 247.07;
+        elementAtomicMass["Co"] = 58.933;
+        elementAtomicMass["Cr"] = 51.996;
+        elementAtomicMass["Cs"] = 132.905;
+        elementAtomicMass["Cu"] = 63.546;
+        elementAtomicMass["Dy"] = 162.5;
+        elementAtomicMass["Er"] = 167.26;
+        elementAtomicMass["Es"] = 252.083;
+        elementAtomicMass["Eu"] = 151.96;
+        elementAtomicMass["F"] = 18.998;
+        elementAtomicMass["Fe"] = 55.847;
+        elementAtomicMass["Fm"] = 257.095;
+        elementAtomicMass["Fr"] = 223.02;
+        elementAtomicMass["Ga"] = 69.723;
+        elementAtomicMass["Gd"] = 157.25;
+        elementAtomicMass["Ge"] = 72.59;
+        elementAtomicMass["H"] = 1.008;
+        elementAtomicMass["He"] = 4.003;
+        elementAtomicMass["Hf"] = 178.49;
+        elementAtomicMass["Hg"] = 200.59;
+        elementAtomicMass["Ho"] = 164.93;
+        elementAtomicMass["I"] = 126.905;
+        elementAtomicMass["In"] = 114.82;
+        elementAtomicMass["Ir"] = 192.22;
+        elementAtomicMass["K"] = 39.098;
+        elementAtomicMass["Kr"] = 83.8;
+        elementAtomicMass["La"] = 138.906;
+        elementAtomicMass["Li"] = 6.941;
+        elementAtomicMass["Lr"] = 260.105;
+        elementAtomicMass["Lu"] = 174.967;
+        elementAtomicMass["Md"] = 258.099;
+        elementAtomicMass["Mg"] = 24.305;
+        elementAtomicMass["Mn"] = 54.938;
+        elementAtomicMass["Mo"] = 95.94;
+        elementAtomicMass["N"] = 14.007;
+        elementAtomicMass["Na"] = 22.99;
+        elementAtomicMass["Nb"] = 92.906;
+        elementAtomicMass["Nd"] = 144.24;
+        elementAtomicMass["Ne"] = 20.179;
+        elementAtomicMass["Ni"] = 58.69;
+        elementAtomicMass["No"] = 259.101;
+        elementAtomicMass["Np"] = 237.048;
+        elementAtomicMass["O"] = 15.999;
+        elementAtomicMass["Os"] = 190.2;
+        elementAtomicMass["P"] = 30.974;
+        elementAtomicMass["Pa"] = 231.036;
+        elementAtomicMass["Pb"] = 207.2;
+        elementAtomicMass["Pd"] = 106.42;
+        elementAtomicMass["Pm"] = 144.913;
+        elementAtomicMass["Po"] = 208.982;
+        elementAtomicMass["Pr"] = 140.908;
+        elementAtomicMass["Pt"] = 195.08;
+        elementAtomicMass["Pu"] = 244.064;
+        elementAtomicMass["Ra"] = 226.025;
+        elementAtomicMass["Rb"] = 85.468;
+        elementAtomicMass["Re"] = 186.207;
+        elementAtomicMass["Rh"] = 102.906;
+        elementAtomicMass["Rn"] = 222.018;
+        elementAtomicMass["Ru"] = 101.07;
+        elementAtomicMass["S"] = 32.066;
+        elementAtomicMass["Sb"] = 121.75;
+        elementAtomicMass["Sc"] = 44.956;
+        elementAtomicMass["Se"] = 78.96;
+        elementAtomicMass["Si"] = 28.086;
+        elementAtomicMass["Sm"] = 150.36;
+        elementAtomicMass["Sn"] = 118.71;
+        elementAtomicMass["Sr"] = 87.62;
+        elementAtomicMass["Ta"] = 180.948;
+        elementAtomicMass["Tb"] = 158.925;
+        elementAtomicMass["Tc"] = 97.907;
+        elementAtomicMass["Te"] = 127.6;
+        elementAtomicMass["Th"] = 232.038;
+        elementAtomicMass["Ti"] = 47.88;
+        elementAtomicMass["Tl"] = 204.383;
+        elementAtomicMass["Tm"] = 168.934;
+        elementAtomicMass["U"] = 238.029;
+        elementAtomicMass["V"] = 50.942;
+        elementAtomicMass["W"] = 183.85;
+        elementAtomicMass["Xe"] = 131.29;
+        elementAtomicMass["Y"] = 88.906;
+        elementAtomicMass["Yb"] = 173.04;
+        elementAtomicMass["Zn"] = 65.39;
+        elementAtomicMass["Zr"] = 91.224;
     }
     cout << "How many elements are in your mixture?" << endl;
-    cin >> n;
+    //cin >> num;
+    num = 2;
     cout << "Enter you chemical substances in different lines:" << endl;
     vector<pair<string, int>> elements;
-    for (int i = 0; i < n; i++) {
-        cin >> substance[i];
-        elements = splitter(substance[i]);
+    for (int i = 0; i < num; i++) {
+        cin >> plug;
+        intoSubstance.push_back(plug);
+        elements = splitter(intoSubstance[i]);
         //printElements(elements);
-        masses[i] = getMass(elements, element_m);
+        massesOfSubstance.push_back(getMass(elements, elementAtomicMass));
     }
     cout << "What value do you need to find?" << endl
          << "Are you entering mole fractions and want to find mass fractions? - 1" << endl
          << "Are you entering mass fractions and want to find mole fractions? - 2" << endl;
     int marker;
     cin >> marker;
-    if (marker == 1) {};
-    if (marker == 2) {};
-
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    // Считываем количество веществ, считываем их формулы, находим молекулярные массы          //
-    // Вводятся мольные доли - находятся массовые ИЛИ вводятся массовые - находятся молярные   //
-    // Массовая доля: w(элемента) = m(элемента) / m(суммв масс всех веществ) * 100             //
-    // Мольная доля: x(элеманта) = n(элемента) / n(сумма количества вещества всех компонентов) //
-    // Количество вещества: n(элемента) = m(элемента) / M(вещества)                            //
-    /////////////////////////////////////////////////////////////////////////////////////////////
-
+    if (marker == 1) massCase(massesOfSubstance, intoSubstance, num);
+    if (marker == 2) moleCase(massesOfSubstance, intoSubstance, num);
     return 0;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Считываем количество веществ, считываем их формулы, находим молекулярные массы          //
+// Вводятся мольные доли - находятся массовые ИЛИ вводятся массовые - находятся молярные   //
+// Массовая доля: w(элемента) = m(элемента) / m(сумма масс всех веществ) * 100             //
+// Мольная доля: x(элеманта) = n(элемента) / n(сумма количества вещества всех компонентов) //
+// Количество вещества: n(элемента) = m(элемента) / M(вещества)                            //
+/////////////////////////////////////////////////////////////////////////////////////////////
